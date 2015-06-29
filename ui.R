33a6dcb337d1d8e47b5f7c565fd24d21e302c679
Julien Barnier <julien@nozav.org>
2015-06-29 17:00:46 +0200

    UI tweak

--- ui.R
+++ ui.R
@@ -2,13 +2,13 @@ library(shiny)
 library(scatterD3)
 
 fluidPage(
-    sidebarLayout(
-      sidebarPanel(
-        numericInput("scatterD3-size", "Labels size :", min = 2, max = 30, value = 10),
-        numericInput("scatterD3-opacity", "Opacity :", min = 0, max = 1, value = 1, step=0.05),
-        actionButton("scatterD3-resetzoom", "Reset Zoom"),
-        tags$a(id="scatterD3-download", href="#", class="btn btn-default", "Download SVG")
-      ),
-      mainPanel(scatterD3Output("scatterPlot"))
-    )
-  )
+  titlePanel("Sample scatterD3 shiny app"),
+  sidebarPanel(
+    numericInput("scatterD3-size", "Labels size :", min = 2, max = 30, value = 10),
+    numericInput("scatterD3-opacity", "Opacity :", min = 0, max = 1, value = 1, step=0.05),
+    actionButton("scatterD3-resetzoom", "Reset Zoom"),
+    tags$a(id="scatterD3-download", href="#", class="btn btn-default", "Download SVG")
+  ),
+  mainPanel(scatterD3Output("scatterPlot"))
+)
+  
