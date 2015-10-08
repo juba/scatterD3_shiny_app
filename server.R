24e93de7a7cef6ba046f9c1401824384634a9802
Julien Barnier <julien@nozav.org>
2015-10-08 14:47:52 +0200

    New version, updatable chart with transitions.

--- server.R
+++ server.R
@@ -2,10 +2,29 @@ library(shiny)
 library(scatterD3)
 
 function(input, output) {
-    output$scatterPlot <- renderScatterD3({
-      scatterD3(x=mtcars$wt,
-                y=mtcars$mpg,
-                lab=rownames(mtcars),
-                col_var=mtcars$cyl)
-    })
-  }
+  
+  data <- reactive({
+    mtcars[1:input$scatterD3_nb,]
+  })
+  
+  output$scatterPlot <- renderScatterD3({
+    col_var <- if (input$scatterD3_col == "None") NULL else data()[,input$scatterD3_col]
+    symbol_var <- if (input$scatterD3_symbol == "None") NULL else data()[,input$scatterD3_symbol]
+    size_var <- if (input$scatterD3_size == "None") NULL else data()[,input$scatterD3_size]
+    scatterD3(x = data()[,input$scatterD3_x],
+              y = data()[,input$scatterD3_y],
+              lab = rownames(data()),
+              xlab = input$scatterD3_x,
+              ylab = input$scatterD3_y,
+              col_var = col_var,
+              col_lab = input$scatterD3_col,
+              symbol_var = symbol_var,
+              symbol_lab = input$scatterD3_symbol,
+              size_var = size_var,
+              size_lab = input$scatterD3_size,
+              key_var = rownames(data()),
+              point_opacity = input$scatterD3_opacity,
+              labels_size = input$scatterD3_labsize,
+              transitions = input$scatterD3_transitions)
+  })
+}
--- ui.R
+++ ui.R
@@ -3,12 +3,75 @@ library(scatterD3)
 
 fluidPage(
   titlePanel("Sample scatterD3 shiny app"),
-  sidebarPanel(
-    numericInput("scatterD3-size", "Labels size :", min = 2, max = 30, value = 10),
-    numericInput("scatterD3-opacity", "Opacity :", min = 0, max = 1, value = 1, step=0.05),
-    actionButton("scatterD3-resetzoom", "Reset Zoom"),
-    tags$a(id="scatterD3-download", href="#", class="btn btn-default", "Download SVG")
+  div(class="row",
+      div(class="col-md-12",
+          div(class="alert alert-warning alert-dismissible",
+              HTML('<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>'),
+              HTML("<strong>What you can try here :</strong>
+<ul>
+<li>Zoom on the chart with the mousewheel</li>
+<li>Pan the plot</li>
+<li>Drag text labels</li>
+<li>Hover over a dot to display tooltips</li>
+<li>Hover over the color or symbol legends items</li>
+<li>Change data settings to see transitions</li>
+<li>Resize the window to test for responsiveness</li>
+</ul>")))),
+  sidebarLayout(
+    sidebarPanel(
+    numericInput("scatterD3_nb", "Number of observations",
+                 min = 1, max = nrow(mtcars), value = nrow(mtcars)),
+    selectInput("scatterD3_x", "x variable :",
+                choices = c("Miles/(US) gallon" = "mpg",
+                            "Displacement (cu.in.)" = "disp",
+                            "Gross horsepower" = "hp",
+                            "Rear axle ratio" = "drat",
+                            "Weight (lb/1000)" = "wt",
+                            "1/4 mile time" = "qsec"),
+                selected = "wt"),
+    selectInput("scatterD3_y", "y variable :",
+                choices = c("Miles/(US) gallon" = "mpg",
+                            "Displacement (cu.in.)" = "disp",
+                            "Gross horsepower" = "hp",
+                            "Rear axle ratio" = "drat",
+                            "Weight (lb/1000)" = "wt",
+                            "1/4 mile time" = "qsec"),
+                selected = "mpg"),
+    selectInput("scatterD3_col", "Color mapping variable :",
+                choices = c("None" = "None",
+                            "Number of cylinders" = "cyl",
+                            "V/S" = "vs",
+                            "Transmission" = "am",
+                            "Number of forward gears" = "gear",
+                            "Number of carburetors" = "carb"),
+                selected = "cyl"),
+    selectInput("scatterD3_symbol", "Symbol mapping variable :",
+                choices = c("None" = "None",
+                            "Number of cylinders" = "cyl",
+                            "V/S" = "vs",
+                            "Transmission" = "am",
+                            "Number of forward gears" = "gear",
+                            "Number of carburetors" = "carb"),
+                selected = "am"),
+    selectInput("scatterD3_size", "Size mapping variable :",
+                choices = c("None" = "None",
+                            "Miles/(US) gallon" = "mpg",
+                            "Displacement (cu.in.)" = "disp",
+                            "Gross horsepower" = "hp",
+                            "Rear axle ratio" = "drat",
+                            "Weight (lb/1000)" = "wt",
+                            "1/4 mile time" = "qsec"),
+                selected = "hp"),
+    numericInput("scatterD3_labsize", "Labels size :",
+                 min = 2, max = 30, value = 10),
+    numericInput("scatterD3_opacity", "Points opacity :", min = 0, max = 1, value = 1, step = 0.05),
+    checkboxInput("scatterD3_transitions", "Use transitions", value = TRUE),
+    tags$p(actionButton("scatterD3-reset-zoom", "Reset Zoom"),
+           tags$a(id = "scatterD3-svg-export", href = "#",
+                  class = "btn btn-default", "Download SVG")),
+    tags$ul(tags$li(tags$a(href = "https://github.com/juba/scatterD3", "scatterD3 on GitHub")),
+            tags$li(tags$a(href = "https://github.com/juba/scatterD3_shiny_app", "This app on GitHub")))
   ),
-  mainPanel(scatterD3Output("scatterPlot"))
-)
-  
+  mainPanel(scatterD3Output("scatterPlot", height = "700px"))
+))
+
